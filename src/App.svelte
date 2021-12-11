b15ab43ad021a629468e099f8a9fa3dbf7d3d551
zubair-saif <zubairsaif700@gmail.com>
2021-12-11 23:40:55 +0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import FeedbackList from "./components/feedbackList.svelte";
+	import FeedbackItems from "./components/feedbackItems.svelte";
+</script>
+
+
+<main>
+	<h1 >Hello </h1>
+	
+</main>
+
+<style>
+	
+</style>
